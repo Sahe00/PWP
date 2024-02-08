6c57659041d35de3a6dd4ee136e6c5787366d634
aemrehu <aemrehu@gmail.com>
2024-02-08 20:14:50 +0200

    add database models

---
+++ app.py
@@ -0,0 +1,35 @@
+import json
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customerId = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    customer = db.relationship('Customer', backref=db.backref('orders', lazy=True))
+
+class Product_order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    orderId = db.Column(db.Integer, db.ForeignKey('order.id'))
+    productId = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    desc = db.Column(db.String(128))
+    price = db.Column(db.Float)
+
+class Stock(db.Model):
+    productId = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
