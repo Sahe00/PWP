ae04787ad682e2f5119679282306b3ae71768de2
apukari <apukari@student.oulu.fi>
2024-02-09 11:48:16 +0200

    default items added to db

--- app.py
+++ app.py
@@ -8,42 +8,42 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(50))
-    lastName = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    phone = db.Column(db.String(50))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    firstName = db.Column(db.String(50), nullable=False)
+    lastName = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=True)
+    phone = db.Column(db.String(50), nullable=True)
 
     orders = db.relationship('Order', back_populates="customer")
 
 class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    customerId = db.Column(db.Integer, db.ForeignKey('customer.id'))
-    createdAt = db.Column(db.String(50))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    customerId = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    createdAt = db.Column(db.String(50), nullable=False)
 
     customer = db.relationship('Customer', back_populates="orders")
     productOrder = db.relationship('ProductOrder', back_populates="order")
 
 class ProductOrder(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    orderId = db.Column(db.Integer, db.ForeignKey('order.id'))
-    productId = db.Column(db.Integer, db.ForeignKey('product.id'))
-    quantity = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    orderId = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    productId = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
     order = db.relationship('Order', back_populates="productOrder")
     product = db.relationship('Product', back_populates="products")
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    desc = db.Column(db.String(128))
-    price = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    desc = db.Column(db.String(128), nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
     products = db.relationship('ProductOrder', back_populates="product")
     stock = db.relationship('Stock', back_populates="stockProduct")
 
 class Stock(db.Model):
-    productId = db.Column(db.Integer, primary_key=True)
+    productId = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True, nullable=False)
     quantity = db.Column(db.Integer)
 
     stockProduct = db.relationship('Product', back_populates="stock")
--- createdatabase.py
+++ createdatabase.py
@@ -1,9 +1,72 @@
+from datetime import datetime
 from app import app
 from app import db
+from app import Customer, Order, Product, Stock, ProductOrder
 
 # This script creates the database and tables. It should be run once before running the app.
 
 ctx = app.app_context()
 ctx.push()
 db.create_all()
+# ctx.pop()
+
+antti = Customer(
+    firstName="Antti",
+    lastName="Heikkinen",
+    email="a.heikkinen@luukku.com",
+    phone="04012345667"
+)
+
+satikka = Product(
+    name="Sateenvarjo",
+    desc="Sateenvarjo suojaa sinua sateelta kuin sateelta!",
+    price=20.00
+)
+
+kumpparit = Product(
+    name="Kumpparit",
+    desc="Kumiset saappaat, pitävät varpaasi kuivana!",
+    price=10.00
+)
+
+antin_tilaus = Order(
+    customer=antti,
+    createdAt=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+)
+
+antin_tuote_1 = ProductOrder(
+    order=antin_tilaus,
+    product=satikka,
+    quantity=2
+)
+
+antin_tuote_2 = ProductOrder(
+    order=antin_tilaus,
+    product=kumpparit,
+    quantity=1
+)
+
+satikkavarasto = Stock(
+    quantity=8,
+    stockProduct=satikka
+)
+
+kumpparivarasto = Stock(
+    quantity=20,
+    stockProduct=kumpparit
+)
+
+db.session.add_all(
+    [
+        antti,
+        satikka,
+        kumpparit,
+        antin_tilaus,
+        antin_tuote_1,
+        antin_tuote_2,
+        satikkavarasto,
+        kumpparivarasto
+    ]
+)
+db.session.commit()
 ctx.pop()
