dd0d29c1ca4f0e6f864d47de0d62be5cbf1ac551
aemrehu <aemrehu@gmail.com>
2024-02-24 15:26:58 +0200

    add collectionclasses, add flask-restful requirement

--- onlinestore/resources/order.py
+++ onlinestore/resources/order.py
@@ -1,3 +1,14 @@
+from flask_restful import Resource
 
+class OrderCollection(Resource):
+    def get(self):
+        pass
 
-# collection and item classes
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def put(self):
+        pass
