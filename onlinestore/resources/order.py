4c2b7bd56c70c52f6ec971e838033d42d2e54b75
aemrehu <aemrehu@gmail.com>
2024-03-01 12:03:15 +0200

    ordercollection get

--- onlinestore/resources/order.py
+++ onlinestore/resources/order.py
@@ -1,8 +1,24 @@
+import json
+from sqlalchemy.exc import IntegrityError
+from flask import Response, request, url_for
 from flask_restful import Resource
+from onlinestore import db
+from onlinestore.models import Customer, Order, ProductOrder, Product, Stock
 
 class OrderCollection(Resource):
+
+    # Returns a list of orders in the database
     def get(self):
-        pass
+        try:
+            result = []
+            for order in db.session.query(Order).all():
+                o_info = {"customerId":"", "createdAt":""}
+                o_info["customerId"] = order.customerId
+                o_info["createdAt"] = order.createdAt
+                result.append(o_info)
+            return result, 200
+        except Exception as e:
+            return f"Error: {e}", 500
 
     def post(self):
         pass
