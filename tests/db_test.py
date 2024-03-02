30e4d2d18726da429195b8dd928bbcbe8126019f
Tomi Pantsar <tomi___00@hotmail.com>
2024-03-02 18:03:11 +0200

    add db_test.py

---
+++ tests/db_test.py
@@ -0,0 +1,122 @@
+import os
+import pytest
+import tempfile
+import time
+from datetime import datetime
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError, StatementError
+
+from onlinestore import create_app, db
+from onlinestore.models import Customer, Order, Product, ProductOrder, Stock
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+# based on http://flask.pocoo.org/docs/1.0/testing/
+# we don't need a client for database testing, just the db handle
+@pytest.fixture
+def app():
+    db_fd, db_fname = tempfile.mkstemp()
+    config = {
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
+        "TESTING": True
+    }
+
+    app = create_app(config)
+
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_fname)
+
+def _get_customer(number=1):
+    return Customer(
+        #uuid="testuuid-{}".format(number),
+        firstName="firstName-{}".format(number),
+        lastName="lastName-{}".format(number),
+        email="email-{}".format(number),
+        phone="phone-{}-123456789".format(number)
+    )
+
+def _get_product(number=1):
+    return Product(
+        name="product-{}".format(number),
+        desc="desc-{}".format(number),
+        price=number * 10.0
+    )
+
+def _get_order(customer):
+    return Order(
+        customerId=customer,
+        createdAt=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
+
+def _get_product_order(order, product, quantity=1):
+    return ProductOrder(
+        order=order,
+        product=product,
+        quantity=quantity
+    )
+
+def _get_stock(stockProduct, quantity=1):
+    return Stock(
+        quantity=quantity,
+        stockProduct=stockProduct
+    )
+
+def test_create_instances(app):
+    """
+    Tests that we can create one instance of each model and save them to the
+    database using valid values for all columns. After creation, test that
+    everything can be found from database, and that all relationships have been
+    saved correctly.
+    """
+
+    with app.app_context():
+        # Create everything
+        customer = _get_customer(1)
+        order = _get_order(customer.id)
+        product = _get_product(1)
+        product_order = _get_product_order(order, product, 1)
+        stock = _get_stock(product, 1)
+
+        #order.location = customer
+        #product_order.sensor = order
+        #stock.sensors.append(order)
+
+        db.session.add(customer)
+        db.session.add(order)
+        db.session.add(product)
+        db.session.add(product_order)
+        db.session.add(stock)
+        db.session.commit()
+
+        # Check that everything exists
+        assert Customer.query.count() == 1
+        assert Order.query.count() == 1
+        assert Product.query.count() == 1
+        assert ProductOrder.query.count() == 1
+        assert Stock.query.count() == 1
+
+        #db_customer = Customer.query.first()
+        #db_order = Order.query.first()
+        #db_product = Product.query.first()
+        #db_product_order = ProductOrder.query.first()
+        #db_stock = Stock.query.first()
+
+        # Check all relationships (both sides)
+        #assert db_customer.orders == db_order
+        #assert db_order.customer == db_customer
+        #assert db_order.productOrder == db_product_order
+        #assert db_product_order.order == db_order
+        #assert db_product_order.product == db_product
+        #assert db_product.products == db_product_order
+        #assert db_product.stock == db_stock
+        #assert db_stock.stockProduct == db_product
